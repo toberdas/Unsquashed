4c97bf47a91fd9bd6b100e1d169122f5764cbaa3
toberdas <tobiasijland@gmail.com>
2021-11-10 16:43:10 +0100

    added an environment and shadows underneath the hands, start to a path patrol function for hands, still have the penetration error show up...

---
+++ bugz/bugMovement.gd
@@ -0,0 +1,108 @@
+extends KinematicBody
+
+var dir := Vector2.ZERO
+var velocity := Vector3.ZERO
+var roty = 0
+var selected = false
+
+onready var timer = $Timer
+onready var aitimer = $AITimer
+onready var animplayer = $AnimationPlayer
+
+var shockwaveCurve = preload("res://shockwave_curve.tres")
+
+enum state {wandering, shockwaved, splatted, dead}
+var _state : int = state.wandering
+var _lastState : int = _state
+
+var impulse = 100
+var drag = 80
+var rotation_speed = 6
+var hp = 1;
+var maxSpeed = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	if _state == state.wandering:
+		get_input()
+		add_velocity(delta)
+		update_rotation(delta)
+		update_position(delta)
+		if dir.length() > .5:
+			animplayer.play("Walk")
+		else:
+			animplayer.stop("Walk")
+	if _state == state.shockwaved:
+		update_rotation(delta)
+		update_position(delta)
+		transform.origin.y = shockwaveCurve.interpolate(1 - timer.time_left)
+		if timer.is_stopped():
+			_state = _lastState
+	if _state == state.splatted:
+		pass
+	if _state == state.dead:
+		pass
+	
+func get_input():
+	if aitimer.is_stopped():
+		dir.x = rand_range(-.2,.2)
+		dir.y = rand_range(-1,-.5)
+		aitimer.start(2)
+
+func update_rotation(delta):
+	roty += ((dir.x * rotation_speed * delta) - roty) /2
+#	transform.basis = Basis() # reset rotation
+	rotate_object_local(Vector3(0, 1, 0), roty)
+	pass
+
+func add_velocity(delta):
+	velocity += transform.basis.z * impulse * dir.y * delta
+	velocity = velocity.move_toward(Vector3.ZERO, drag * delta)
+	velocity.x = clamp(velocity.x, -maxSpeed, maxSpeed)
+	velocity.z = clamp(velocity.z, -maxSpeed, maxSpeed)
+	pass
+	
+func update_position(delta):
+	move_and_slide(velocity)
+	transform = transform.orthonormalized()
+	pass
+	
+
+func splatted(bug):
+	var index = get_index()
+	if bug == index:
+		if _state != state.splatted && _state != state.dead:
+			print("splatted")
+			animplayer.play("Hit")
+			_state = state.splatted
+			hp -= 1
+			if hp == 0:
+				bug_death()
+			
+
+func shockwaved(bug):
+	var index = get_index()
+	if bug == index:
+		print("shockwaved")
+		if _state != state.shockwaved && _state != state.splatted && _state != state.dead:
+			_lastState = _state
+			_state = state.shockwaved
+			timer.start(1)
+
+func bug_death():
+	add_to_group("Dead")
+	_state = state.dead
+	velocity = Vector3.ZERO
+	animplayer.seek(animplayer.get_current_animation_length(), true)
+
+func selected(bug):
+	print("selected")
+	selected = true
+	pass
+
+func recover():
+	_state = state.wandering
--- chasers/chaseScript.gd
+++ chasers/chaseScript.gd
@@ -1,15 +1,22 @@
 extends KinematicBody
+
+export (NodePath) var patrol_path
+var patrol_points
+var patrol_index = 0
+
 var curve = preload("res://stomp_curve.tres")
+
 onready var area = $Area
-onready var area2 = $Area2
+onready var shockwaveArea = $Area2
+onready var shockwaveCollider = $Area2/CollisionShape
 onready var area3 = $Area3
 onready var patroltimer = $Timer
 onready var animplayer = $AnimationPlayer
 
-var speed = 600
+var speed = 900
 var stompDistance = 3
 
-enum state {idle, chasing, waiting, stomping}
+enum state {idle, patrol, chasing, waiting, stomping}
 var _state : int = state.idle
 
 var velocity = Vector3.ZERO
@@ -21,9 +28,11 @@ var stompMultiplier = 1
 var chasingBug = null
 
 func _ready():
+	if patrol_path:
+		patrol_points = get_node(patrol_path).curve.get_baked_points()
 	height = transform.origin.y
 	area.connect("body_entered", self, "bug_detected")
-	area2.connect("body_entered", self, "bug_shockwaved")
+	shockwaveArea.connect("body_entered", self, "bug_shockwaved")
 	area3.connect("body_entered", self, "bug_splatted")
 	pass
 
@@ -32,6 +41,8 @@ func _process(delta):
 	if _state == state.idle:
 		patrol()
 		animplayer.play("Idle")
+	if _state == state.patrol:
+		pass
 	if _state == state.chasing:
 		raycasting()
 		animplayer.play("Chase")
@@ -39,11 +50,23 @@ func _process(delta):
 		animplayer.play("Splat")
 
 func patrol():
-	if patroltimer.is_stopped():
-		moveTarget.x = randi() % 20   
-		moveTarget.z = randi() % 20   
-		patroltimer.start(2)
-
+	if !patrol_path:
+		return
+	moveTarget.x = patrol_points[patrol_index].x
+	moveTarget.z = patrol_points[patrol_index].z
+	var position = transform.origin
+	var distance = position.distance_to(moveTarget)
+	if distance < 1:
+		patrol_index = wrapi(patrol_index + 1, 0, patrol_points.size())
+		moveTarget.x = patrol_points[patrol_index].x
+		moveTarget.z = patrol_points[patrol_index].z
+	pass
+	
+func raycast(from, to):
+	var _direct_state = get_world().direct_space_state
+	var raycol = _direct_state.intersect_ray(from, to)
+	return raycol
+	
 func raycasting():
 	var _direct_state = get_world().direct_space_state
 	var raycol = _direct_state.intersect_ray(transform.origin, chasingBug.transform.origin)
@@ -54,18 +77,22 @@ func raycasting():
 			if Vector2(transform.origin.x,transform.origin.z).distance_to(Vector2(moveTarget.x,moveTarget.z)) < stompDistance:
 					_state = state.stomping
 		else:
-			_state = state.idle
+			_state = state.chasing
 
 func bug_detected(bug):
 	if bug.is_in_group("Bugs") && !bug.is_in_group("Dead"):
-		print("bug detected")
-		chasingBug = bug
-		_state = state.chasing
+		var raycastbug = raycast(transform.origin, bug.transform.origin)
+		if raycastbug.collider.is_in_group("Bugs") && !bug.is_in_group("Dead"):
+			print("bug detected")
+			chasingBug = bug
+			_state = state.chasing
 	pass
 
-
+func bug_splatted(bug):
+	get_tree().call_group("Bugs", "splatted", bug.get_index())
 
 func bug_shockwaved(bug):
+	shockwaveCollider.set_disabled(true)
 	get_tree().call_group("Bugs", "shockwaved", bug.get_index())
 
 func hand_up():
@@ -75,6 +102,7 @@ func hand_up():
 	_state = state.idle
 
 func hand_down():
+	shockwaveCollider.set_disabled(false)
 	print("Stomp!")
 	moveTarget.y = 0
 	stomping = true
